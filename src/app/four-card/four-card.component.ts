6cd94437142855bd70f3ade0c22241336fe23e32
feng <zzhxufeng@163.com>
2025-01-07 22:26:13 +0800

    grid layout

--- src/app/four-card/four-card.component.ts
+++ src/app/four-card/four-card.component.ts
@@ -18,25 +18,29 @@ export class FourCardComponent {
   poweredBy = 'Powered by Technology';
   intro =
     'Our Artificial Intelligence powered tools use millions of project data points to ensure that your project is successful';
-  supervisor: Model = {
-    title: 'Supervisor',
-    intro: 'Monitors activity to identify project roadblocks',
-    url: 'images/icon-supervisor.svg',
-  };
-  teamBuilder: Model = {
-    title: 'Team Builder',
-    intro:
-      'Scans our talent network to create the optimal team for your project',
-    url: 'images/icon-team-builder.svg',
-  };
-  karma: Model = {
-    title: 'Karma',
-    intro: 'Regularly evaluates our talent to ensure quality',
-    url: 'images/icon-karma.svg',
-  };
-  calculator: Model = {
-    title: 'Calculator',
-    intro: 'Uses data from past projects to provide better delivery estimates',
-    url: 'images/icon-calculator.svg',
-  };
+
+  models: Model[] = [
+    {
+      title: 'Supervisor',
+      intro: 'Monitors activity to identify project roadblocks',
+      url: 'images/icon-supervisor.svg',
+    },
+    {
+      title: 'Team Builder',
+      intro:
+        'Scans our talent network to create the optimal team for your project',
+      url: 'images/icon-team-builder.svg',
+    },
+    {
+      title: 'Karma',
+      intro: 'Regularly evaluates our talent to ensure quality',
+      url: 'images/icon-karma.svg',
+    },
+    {
+      title: 'Calculator',
+      intro:
+        'Uses data from past projects to provide better delivery estimates',
+      url: 'images/icon-calculator.svg',
+    },
+  ];
 }
